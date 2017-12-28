a81f8360bf749b41eef7964f17ff3cb472a94f34
Daniel  Conte <danielconte@MacBook-Pro-de-Daniel.local>
2017-12-28 08:59:17 -0200

    FEAT: Closes #28, Add Login Page

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +10,11 @@ import { LoginPage } from './login';
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
+    ReactiveFormsModule,
+    TranslateModule.forChild()
   ],
+  exports: [
+    LoginPage
+  ]
 })
 export class LoginPageModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,25 +1,81 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { 
+  IonicPage, 
+  NavController, 
+  MenuController,
+  LoadingController, 
+  Loading, 
+  AlertController,
+  App } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+// import { AuthProvider } from '../../providers';
+// import { EmailValidator } from '../../validators/email';
+// import { AppConfig } from './../../app/config/app.config';
 
-@IonicPage()
+@IonicPage({
+    name: 'login',
+    segment: 'login'
+})
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  user: FormGroup; // = {} as IUser;
+  loading: Loading;
+
+  constructor(
+    private fb: FormBuilder,
+    public navCtrl: NavController,
+    // public authData: AuthProvider,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    public menu : MenuController,
+    private _app: App,
+    // public config: AppConfig
+  ) {
+    this.user = this.fb.group({
+      email: ['', Validators.compose([Validators.required])], //, EmailValidator.isValid
+      password: ['', Validators.compose([Validators.required])]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  ionViewWillLeave(){
+    this.menu.enable(true);
+  }
+  
+  ionViewWillEnter(){
+    this.menu.enable(false);
   }
 
+  loginUser(){
+      // if (!this.user.valid){
+      //   console.log(this.user.value);
+      // } else {
+        // this.authData.loginUser(this.user.value.email, this.user.value.password)
+        // .then( authData => {
+        //   this.navCtrl.setRoot('sites-list');
+        // }, error => {
+        //   this.loading.dismiss().then( () => {
+        //     let alert = this.alertCtrl.create({
+        //       message: error.message,
+        //       buttons: [
+        //         {
+        //           text: "Ok",
+        //           role: 'cancel'
+        //         }
+        //       ]
+        //     });
+        //     alert.present();
+        //   });
+        // });
+
+        // this.loading = this.loadingCtrl.create({
+        //   dismissOnPageChange: true,
+        // });
+        // this.loading.present();
+      // }
+  }
 }
