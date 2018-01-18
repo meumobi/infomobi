be768d30c0c4fdd5938bd4479bdd4cfe5cd6fbe5
Daniel Conte <daniel.conte@meumobi.com>
2018-01-18 14:06:43 -0200

    FEAT: Gulp Automation to Release/Build

---
+++ PROJECTS/meumobi/config/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBnCsnH9XAtG73lIwRuYj4dbXLyPBv0E5I",
+  authDomain: "ion-employee.firebaseapp.com",
+  databaseURL: "https://ion-employee.firebaseio.com",
+  projectId: "ion-employee",
+  storageBucket: "ion-employee.appspot.com",
+  messagingSenderId: "961286969528",
+  project: "meumobi"
+};
