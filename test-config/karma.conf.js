d7b8e4205459f8f70c0c3ecb1149d78c88f7b1a2
victor dias <victor.dias@meumobi.com>
2018-05-17 19:06:15 +0100

    FIX: try to fix on travis env 'Cannot start Chrome' adding dist: trusty

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function(config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'ChromeHeadless'],
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
