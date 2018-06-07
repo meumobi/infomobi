91923d115ef9d03d916b3c26c4f413b0ee4d2556
victor dias <victor.dias@meumobi.com>
2018-06-08 00:11:15 +0100

    fix: prevent test failed because of translationService

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { async, TestBed } from '@angular/core/testing';
 import { IonicModule, Platform } from 'ionic-angular';
 
+import {HttpClient} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AnalyticsProvider } from '@shared/analytics.service';
+import { AnalyticsProvider } from '../shared/analytics.service';
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
 
 import { MyApp } from './app.component';
+import { createTranslateLoader } from "./app.module";
 
 import {} from 'jasmine';
 import {
@@ -25,15 +29,24 @@ describe('MyApp Component', () => {
     TestBed.configureTestingModule({
       declarations: [MyApp],
       imports: [
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: createTranslateLoader,
+            deps: [HttpClient]
+          }
+        })
       ],
       providers: [
+        TranslateService,
         { provide: StatusBar, useClass: StatusBarMock },
         { provide: SplashScreen, useClass: SplashScreenMock },
         { provide: Platform, useClass: PlatformMock },
         { provide: AnalyticsProvider, useClass: AnalyticsMock }
       ]
-    })
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -48,5 +61,4 @@ describe('MyApp Component', () => {
   it('should have two pages', () => {
     expect(component.pages.length).toBe(2);
   });
-
 });
