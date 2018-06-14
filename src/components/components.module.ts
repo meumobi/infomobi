5c4b03abcffa96d407a72db33b6d3f4314bb1b01
victor dias <victor.dias@meumobi.com>
2018-06-14 02:00:34 +0100

    fix: remove useless module raising error

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { FileUploadComponent } from './file-upload/file-upload';
-@NgModule({
-	declarations: [FileUploadComponent],
-	imports: [],
-	exports: [FileUploadComponent]
-})
-export class ComponentsModule {}
