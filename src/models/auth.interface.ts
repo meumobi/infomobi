65e29a3887cb5cf893b028c219de0d2f2b10447d
victor dias <victor.dias@meumobi.com>
2018-07-05 06:45:22 +0100

    refactor: update interface auth

--- src/models/auth.interface.ts
+++ src/models/auth.interface.ts
@@ -2,10 +2,12 @@ export interface Auth {
   success: boolean,
   token: string,
   message?: string,
-  visitor: {
-    first_name: string,
-    last_name: string,
-    email: string,
-    site: string
-  },
+  visitor: User
+}
+
+export interface User {
+  first_name: string,
+  last_name: string,
+  email: string,
+  site: string
 }
