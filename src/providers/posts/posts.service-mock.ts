85f1b928b433b87ead3e31343faf42b91acd9067
victor dias <victor.dias@meumobi.com>
2018-07-19 02:37:42 +0100

    feat: fetch posts from meumobi api

--- src/providers/posts/posts.service-mock.ts
+++ src/providers/posts/posts.service-mock.ts
@@ -2,16 +2,17 @@ import { Injectable } from '@angular/core';
 
 import posts from './mock-posts';
 import Utils from '@shared/utils';
+import { Post } from '@models/post.interface';
 
 @Injectable()
 export class PostsProvider {
 
-  findAll() {
-    return Promise.resolve(posts);
+  findAll():Promise<Post[]> {
+    return Promise.resolve(posts.items);
   }
 
-  findById(id) {
-    let data = Utils.lookup(posts);
+  findById(id):Promise<Post> {
+    let data = Utils.lookup(posts.items);
     return Promise.resolve(data[id]);
   }
 
--- src/providers/posts/posts.service.ts
+++ src/providers/posts/posts.service.ts
@@ -1,17 +1,41 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { ENV } from '@env';
+import { Post } from '@models/post.interface';
+import posts from './mock-posts';
+import Utils from '@shared/utils';
 
-/*
-  Generated class for the PostsProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
-export class PostsProvider {
-
+export class PostsProvider implements OnInit {
+  
   constructor(public http: HttpClient) {
     console.log('Hello PostsProvider Provider');
   }
-
+  
+  ngOnInit(): void {
+    
+  }
+  
+  findById(id):Promise<Post> {
+    let data = Utils.lookup(posts);
+    return Promise.resolve(data[id]);
+  }
+  
+  findAll():Promise<Post[]> {
+    const httpOptions = {
+      headers: {
+        'Accept':  'application/json',
+        'X-Visitor-Token': '94086d89cce67659fe83eb72a548cd5707e6a800'
+      }
+    };
+    
+    const url = ENV.meumobi.apiUrl + "/api/katrium.meumobi.com/items/latest";
+    
+    return this.http.get(url, httpOptions).toPromise()
+    .then(
+      (res: any) => {
+        return res.items;
+      }
+    );
+  }
 }
