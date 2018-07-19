23d4412f1aa4ec4ac1fd71378c967cc7159b0240
Daniel Conte <daniel.conte@meumobi.com>
2018-07-19 12:09:36 -0300

    update auth mock to have the same real service interface

--- src/providers/auth/auth.service-mock.ts
+++ src/providers/auth/auth.service-mock.ts
@@ -1,50 +1,18 @@
 import { Injectable } from '@angular/core';
-import auth from './mock-auth';
-import { Auth } from '@models/auth.interface';
 
 @Injectable()
-export class AuthProvider {
+export class AuthService {
   
   constructor() {
     console.log('Hello AuthProvider Provider');
-  }
-  
-  loginUser(email: string, password: string): Promise<Auth> {
-    
-    /* Dummy authentication for testing, uses $timeout to simulate api call
-    ----------------------------------------------*/
-    
-    return new Promise(resolve =>
-      setTimeout(() => {
-        let response = auth;
-  
-        if(!response.success) {
-          response['message'] = 'Username or password is incorrect';
-        }
-  
-        resolve(response);
-      }, 1000)
-    );    
-  }
-  
-  resetPassword(email: string): Promise<any> {
+  }  
+
+  signIn(email: string, password: string): Promise<any> {
     return Promise.resolve();
   }
-  
-  logoutUser(): Promise<any> {
-    return Promise.resolve();
-  }
-  
-  clearCredentials(): Promise<any> {
-    return Promise.resolve();
-  }
-  
-  setCredentials(): Promise<any> {
-    return Promise.resolve();
-  }
-  
-  isAuthenticated(): Promise<any> {
-    return Promise.resolve();
-  }
+
+  signOut() {}
+
+  isAuthenticated() {}
   
 }
