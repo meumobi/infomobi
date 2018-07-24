eeedfbd4190dacf763ae2a465d4b6a46fe27652c
Daniel Conte <daniel.conte@meumobi.com>
2018-07-24 11:39:09 -0300

    partial solution

--- src/providers/auth-data-presistence/auth-data-persistence.service.ts
+++ src/providers/auth-data-presistence/auth-data-persistence.service.ts
@@ -1,32 +1,46 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from "rxjs";
+import { Injectable, OnInit } from '@angular/core';
+import { Subject } from "rxjs";
 import { Auth } from '@models/auth.interface';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class AuthDataPersistenceService {
-  
-  isLoggedSubject = new BehaviorSubject<boolean>(this.hasToken());
+  isLoggedSubject = new Subject<boolean>();
+
+  constructor(
+    private storage: Storage
+  ) {
+    this.hasToken()
+    .then(data => this.isLoggedSubject.next(data));
+  }
   
   /**
   * if we have token the user is loggedIn
   * @returns {boolean}
   */
-  private hasToken(): boolean {
-    return !!localStorage.getItem('authData');
+  private hasToken(): Promise<boolean> {
+    console.log('toke');
+    return this.storage.get("authData")
+    .then(
+      data => !!JSON.parse(data)
+    )
   }
   
-  get(): Auth {
-    return JSON.parse(localStorage.getItem('authData'));
+  get(): Promise<Auth> {
+    console.log("get");
+    return this.storage.get("authData")
+    .then(data => JSON.parse(data))
   }
   
   set(auth): void {
     console.log('set authData on localStorage');
-    localStorage.setItem('authData', JSON.stringify(auth));
-    this.isLoggedSubject.next(true);
+    this.storage.set('authData', JSON.stringify(auth))
+    .then(data => this.isLoggedSubject.next(true));
   }
   
   clear(): void {
-    localStorage.removeItem('authData');
-    this.isLoggedSubject.next(false);
+    console.log("clear");
+    this.storage.remove('authData')
+    .then(data => this.isLoggedSubject.next(false));
   }
 }
