1166d53b172b84112a06dd89acc6cb2dc6dd6e6f
Daniel Conte <daniel.conte@meumobi.com>
2018-08-02 11:31:48 -0300

    feat: add FAT to add new desk/user profile

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { ContactsService } from '@providers/contacts/';
 import { ContactProfile } from '@models/contact-profile';
 import { BehaviorSubject } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
 
 @IonicPage({
   segment: 'contacts'
@@ -16,11 +17,14 @@ export class ContactsPage {
   contacts: Array<ContactProfile>;
   rootNavCtrl: NavController;
   private searchTerm =new BehaviorSubject<string>('');
+  admin: boolean = false;
 
   constructor(
     private contactsService: ContactsService,
     public navCtrl: NavController, 
-    public navParams: NavParams
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    private translateService: TranslateService
   ) {
     this.rootNavCtrl = navParams.get('rootNavCtrl');
   }
@@ -29,7 +33,31 @@ export class ContactsPage {
     this.searchTerm.next(term);
   }
 
+  addContact() {
+    const alert = this.alertCtrl.create({
+      title:  this.translateService.instant('Delete'),
+      message: this.translateService.instant('Do you want to create a User or Desk contact?'),
+      buttons: [
+        {
+          text: this.translateService.instant('User'),
+          handler: () => {
+            this.rootNavCtrl.push("UserFormPage", {new: true});
+          }
+        },
+        {
+          text: this.translateService.instant('Desk'),
+          handler: () => {
+            this.rootNavCtrl.push("DeskFormPage");
+          }
+        }
+      ]
+    });
+    alert.present(); 
+  }
+
   ngOnInit() {
+    // this.admin = currentuser.role == admin
+    this.admin = true;
     this.search();
     this.searchTerm
     .debounceTime(300)
