29540777b675034a9212764a549cc5797fe5140a
Daniel Conte <daniel.conte@meumobi.com>
2018-08-02 10:55:24 -0300

    feat: call edit user-profile from details

--- src/components/contacts/types/user/user.ts
+++ src/components/contacts/types/user/user.ts
@@ -1,8 +1,30 @@
 import { Component, Input } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
 @Component({
   selector: 'user',
   templateUrl: 'user.html'
 })
 export class UserComponent {
   @Input() contact: any;
+  rootNavCtrl: NavController;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+  ) {
+    this.rootNavCtrl = navParams.get('rootNavCtrl') || this.navCtrl;
+  }
+
+  pushDetailsPage(page: string, id: string) {
+    if (id) {
+      this.rootNavCtrl.push(page, {
+        id: id,
+        rootNavCtrl: this.rootNavCtrl
+      });
+    } else {
+      console.log("missing id of author");
+    }
+  }
 }
+
