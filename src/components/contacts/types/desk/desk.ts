e6d18026bc4a109eba3544997097ff44fc93421b
Daniel Conte <daniel.conte@meumobi.com>
2018-08-02 11:17:50 -0300

    feat: call desk-form edit from details

--- src/components/contacts/types/desk/desk.ts
+++ src/components/contacts/types/desk/desk.ts
@@ -1,8 +1,32 @@
 import { Component, Input } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
 @Component({
   selector: 'desk',
   templateUrl: 'desk.html'
 })
 export class DeskComponent {
   @Input() contact: any;
+  rootNavCtrl: NavController;
+  admin: boolean = false;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+  ) {
+    this.rootNavCtrl = navParams.get('rootNavCtrl') || this.navCtrl;
+    // this.admin = currentuser.role == admin;
+    this.admin = true;
+  }
+
+  pushDetailsPage(page: string, id: string) {
+    if (id) {
+      this.rootNavCtrl.push(page, {
+        id: id,
+        rootNavCtrl: this.rootNavCtrl
+      });
+    } else {
+      console.log("missing id of author");
+    }
+  }
 }
