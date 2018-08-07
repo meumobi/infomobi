8ba0814f843fada4560a9f2cbdba5bf8593f52df
victor dias <victor.dias@meumobi.com>
2018-08-07 22:26:47 +0100

    refactor: Closes #143, unsubscribe auth-data observable when logout

--- src/providers/auth-data-presistence/auth-data-persistence.service-mock.ts
+++ src/providers/auth-data-presistence/auth-data-persistence.service-mock.ts
@@ -1,37 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Subject } from "rxjs";
+import { Subject, Observable } from "rxjs";
 import { Auth } from '@models/auth.interface';
 import authData from '@providers/auth/mock-auth.ts';
 
 @Injectable()
 export class AuthDataPersistenceService {
   
-  isLoggedSubject = new Subject<boolean>();
+  private authData$ = new Subject<Auth>();
   
   constructor() {
-    this.hasToken()
-    .then(data => this.isLoggedSubject.next(data));
+    this.authData$.next(authData);
+  }
+  
+  public set(auth): Promise<void> {
+    console.log('set authData on ionicStorage');
+    return Promise.resolve().then( () => this.authData$.next(auth));
   }
 
-  /**
-  * if we have token the user is loggedIn
-  * @returns {boolean}
-  */
- private hasToken(): Promise<boolean> {
-    return Promise.resolve(authData).then(
-      data => !!data
-    );
+  public get(): Observable<Auth> {
+    return this.authData$.asObservable().share();
   }
   
-  public get(): Promise<Auth> {
-    return Promise.resolve(authData);
-  }
-  
-  public set(auth): void {
-    console.log('set authData on ionicStorage');
-  }
-  
-  public clear(): void {
+  public clear(): Promise<void> {
     console.log("clear authData on ionicStorage");
+    return Promise.resolve().then( () => this.authData$.next(null));
   }
 }
