446443255edffd47c7dba1cea3a6a03fed4a238c
Daniel Conte <daniel.conte@meumobi.com>
2018-09-03 13:59:18 -0300

    update model structure

--- src/models/item.interface.ts
+++ src/models/item.interface.ts
@@ -1,19 +1,32 @@
-export interface Polls {
+export interface Item {  
   _id: string,
+
+  images?: Array<any>,
+  link: string,
   medias: Array<any>,
   parent_id: number,
-  images?: Array<any>,
+  pubdate?: number,
   thumbnails?: Array<any>,
   title: string,
   type: string,
   created: number,
+  format?: string,
   groups: Array<any>,
   guid: string,
   is_published?: boolean,
   modified: number,
+  notification_id: string,
   order: number,
   published: number,
-  site_id: number,
+  site_id: number  
+}
+
+export interface Articles extends Item {
+  author: string,
+  description: string,
+}
+
+export interface Polls extends Item {
   end_date: number,
   multiple_choices: boolean,
   options: Array<string>,
