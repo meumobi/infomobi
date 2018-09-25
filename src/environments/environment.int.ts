57d3f61789e847bdadbcedbeb861bfee99d8cd35
Daniel Conte <daniel.conte@meumobi.com>
2018-09-25 09:39:43 -0300

    fix: closes #190, Setup INT environment

---
+++ src/environments/environment.int.ts
@@ -0,0 +1,31 @@
+export const ENV = {
+  production: false,
+  isDebugMode: true,
+  analyticsTrackingId: "UA-113326323-1",
+  meumobi: {
+    apiUrl: 'https://meumobi.com'
+  },
+  imgServer: {
+    url: "https://imageflow.meumobi.com/",
+    sources: {
+      firebase: {
+        prefix: "https://infomobi.page.link"
+      },
+      meumobi: {
+        prefix: "/uploads"
+      }
+    }
+  } , 
+  dynamicLinks: {
+    url: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks",
+    prefix: "https://infomobi.page.link"
+  },
+  firebase: {
+    apiKey: "AIzaSyBemLnl_Wz4U50lD3gXirvjDk5jLUbyA_M",
+    authDomain: "ion-employee-int.firebaseapp.com",
+    databaseURL: "https://ion-employee-int.firebaseio.com",
+    projectId: "ion-employee-int",
+    storageBucket: "ion-employee-int.appspot.com",
+    messagingSenderId: "236514035211"
+  }  
+};
