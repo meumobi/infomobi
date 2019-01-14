1a6d53f8ba0b3c31e25c610c2eb41da79816f046
Daniel Antonio Conte <danconte72@gmail.com>
2019-01-14 08:36:03 -0200

    refactor: auth interface tslint fix

--- src/models/auth.interface.ts
+++ src/models/auth.interface.ts
@@ -1,17 +1,17 @@
 export interface Auth {
-  success: boolean,
-  token: string,
-  message?: string,
-  visitor: AuthUser
+  success: boolean;
+  token: string;
+  message?: string;
+  visitor: AuthUser;
 }
 
 export interface AuthUser {
-  first_name: string,
-  last_name: string,
-  email: string,
-  site: string
+  first_name: string;
+  last_name: string;
+  email: string;
+  site: string;
 }
 
 export class AuthError {
-  message: string
+  message: string;
 }
