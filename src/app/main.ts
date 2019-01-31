5c171d42004fb9d17a4eff055ca4d774b7049053
Daniel Antonio Conte <danconte72@gmail.com>
2019-01-31 11:09:22 -0200

    fix(push): enable sw when user or browser denied permission
    closes #251, closes #249

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,8 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-export function isProdMode():boolean {
-  return process.env.IONIC_ENV == 'prod';
+export function isProdMode(): boolean {
+  return process.env.IONIC_ENV === 'prod';
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
@@ -12,6 +12,10 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(() => {
     navigator.serviceWorker.register('/OneSignalSDKWorker.js')
       .then(() => console.log('service worker installed'))
       .catch(err => console.error('Error', err));
+  }
+
+  if ('serviceWorker' in navigator && !navigator.serviceWorker.controller && isProdMode()) {
+    navigator.serviceWorker.register('/ngsw-worker.js');
   }
 
 }).catch(error => console.log(error));
