fe0d8865ef49d1dc402104dce7098937be5d2593
Daniel Antonio Conte <danconte72@gmail.com>
2019-02-21 13:21:21 -0300

    fix(comments): only admin can promote comments
    closes #291

--- src/directives/directives.module.ts
+++ src/directives/directives.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { HasRoleDirective } from './has-role';
 import { IsAdminDirective } from './is-admin';
 import { CanEditItemDirective } from './can-edit-item';
+import { CanPromoteMessageDirective } from './can-promote-message';
 @NgModule({
   declarations: [
     HasRoleDirective,
     IsAdminDirective,
     CanEditItemDirective,
+    CanPromoteMessageDirective,
   ],
   imports: [],
   exports: [
     HasRoleDirective,
     IsAdminDirective,
     CanEditItemDirective,
+    CanPromoteMessageDirective
   ]
 })
 export class DirectivesModule {}
