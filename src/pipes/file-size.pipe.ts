ee99b9884a3a93668ca7f674b4a2db632dcbbd80
victor dias <victor.dias@meumobi.com>
2019-03-12 15:16:34 +0000

    add mention to author of src code

--- src/pipes/file-size.pipe.ts
+++ src/pipes/file-size.pipe.ts
@@ -1,6 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 /*
+ * @author Jon Catmull
+ * https://gist.github.com/JonCatmull/ecdf9441aaa37336d9ae2c7f9cb7289a
+ *
  * Convert bytes into largest possible unit.
  * Takes an precision argument that defaults to 2.
  * Usage:
